0076a23ab02ffd5babca9f05f6a43ae8bb5e6471
Jupil Hwang <jupil.hwang@gmail.com>
2019-11-06 21:06:13 +0900

    chagned

---
+++ simple-spring-app-01/src/main/kotlin/com/example/simplespringapp/DemoApplication.kt
@@ -0,0 +1,13 @@
+package com.example.simplespringapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+
